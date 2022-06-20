fc1586e92906f9642d9c0420e41eb7c14059a50d
dpk258 <dpk258@yahoo.com>
2022-06-20 18:45:12 +0530

    initial commit

---
+++ src/firstpakage/first.java
@@ -0,0 +1,16 @@
+package firstpakage;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ student std = new student();
+ std.name = "Deepak";
+ System.out.println(std.name);
+	}
+
+}
+class student {
+	String name ;
+	
+}
